fdf9c14872b2bd0842a7b29fc09242dbcf7e2315
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-02-28 14:21:26 +0300

    Assignment completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = []
+    for num in num_list:
+        #Check if the number is even
+       if num % 2 == 0:
+           #To append to the list a number is even
+        even_numbers.append(num)
+    return even_numbers
+
+sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(return_evens(sequence))        
+pass
 
 def make_exclamation(sentence_list):
-    pass
+    #To add an exclamation mark to each sentence in the list
+    return [sentence + '!' for sentence in sentence_list]
+
+sentences = ["Hello", "I'm doing great", "Python is fun"]
+print(make_exclamation(sentences))
+pass
